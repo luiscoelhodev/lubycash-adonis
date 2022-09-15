818814bd451abe5002b0091c1d7c57bce45e1066
Luis Coelho <mancoeluis@gmail.com>
2022-09-15 15:37:54 -0300

    ✨ add migrations

---
+++ database/migrations/1663264711788_addresses.ts
@@ -0,0 +1,28 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'addresses'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE').notNullable()
+
+      table.string('address').notNullable()
+      table.string('city', 50).notNullable()
+      table.string('state', 2).notNullable()
+      table.string('zip_code', 10).notNullable()
+      table.string('complement').nullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
